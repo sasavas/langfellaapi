0b4054b9948a61b566bcfde1a795e593ad8b74d6
Alp Savas <alpsavash@gmail.com>
2024-08-26 18:44:43 +0300

    chore: remove unnecessary files

--- src/main/java/com/zenkodyazilim/langfella/inventory/InventoryService.java
+++
@@ -1,5 +0,0 @@
-package com.zenkodyazilim.langfella.inventory;
-
-public interface InventoryService {
-    boolean isAvailable(String item, int quantity);
-}
--- src/main/java/com/zenkodyazilim/langfella/order/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zenkodyazilim.langfella.order;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface OrderRepository extends JpaRepository<Order, Long> {
-}
