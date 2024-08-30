fde3a7e459d1a1e94f6a7320dd6a8f09b2ec7aa6
Alp Savas <alpsavash@gmail.com>
2024-08-31 01:44:38 +0300

    refactor: change package name from docs to swagger
    custom SecurityFilterChain
    custom UserDetailsService
    custom AuthenticationProvider

--- src/main/java/com/zenkodyazilim/langfella/common/swagger/SwaggerConfig.java
+++ src/main/java/com/zenkodyazilim/langfella/common/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.zenkodyazilim.langfella.common.docs;
+package com.zenkodyazilim.langfella.common.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
